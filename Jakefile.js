de82862c8af71983a75297a6f7da86d738e78bcb
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-12-29 23:43:20 +0100

    Include libjpeg/libpng/zlib
    Closes #1

--- Jakefile.js
+++ Jakefile.js
@@ -1,12 +1,27 @@
 const child_process = require("child_process");
 const path = require("path");
 const fs = require("fs/promises");
+const { lookpath } = require("lookpath");
 
 const BUILD_DIR = process.env.BUILD_DIR || "build";
 const MYDIR = path.resolve(__dirname, "submodules/jpeg-xl/");
 const CMAKE_BUILD_TYPE="Release";
 
-function configure() {
+async function getEmsdkPath() {
+  const emccPath = await lookpath("emcc");
+  if (!emccPath) {
+    throw new Error("Cannot detect emcc");
+  }
+  return path.dirname(emccPath);
+}
+
+async function configure() {
+  const EMSCRIPTEN_WASM_CACHE_DIR = path.resolve(await getEmsdkPath(), "cache/wasm/");
+  const LIBJPEG_DIR = path.resolve(EMSCRIPTEN_WASM_CACHE_DIR, "libjpeg.a");
+  const LIBPNG_DIR = path.resolve(EMSCRIPTEN_WASM_CACHE_DIR, "libpng.a");
+  const ZLIB_DIR = path.resolve(EMSCRIPTEN_WASM_CACHE_DIR, "libz.a");
+  const LIB_INCLUDE = path.resolve(EMSCRIPTEN_WASM_CACHE_DIR, "include") + "/";
+
   const args = [
     `-B"${BUILD_DIR}"`,
     `-H"${MYDIR}"`,
@@ -16,20 +31,31 @@ function configure() {
     // See https://github.com/emscripten-core/emscripten/issues/13126
     `-DCMAKE_CROSSCOMPILING_EMULATOR="${process.execPath}"`,
     '-DJPEGXL_STATIC=ON',
+    `-DJPEG_LIBRARY="${LIBJPEG_DIR}"`,
+    `-DJPEG_INCLUDE_DIR="${LIB_INCLUDE}"`,
+    `-DPNG_LIBRARY="${LIBPNG_DIR}"`,
+    `-DPNG_PNG_INCLUDE_DIR="${LIB_INCLUDE}"`,
+    `-DZLIB_LIBRARY="${ZLIB_DIR}"`,
+    `-DZLIB_INCLUDE_DIR="${LIB_INCLUDE}"`,
     `-DBUILD_TESTING=OFF`,
     `-DCMAKE_EXE_LINKER_FLAGS="-s ALLOW_MEMORY_GROWTH=1 -s NODERAWFS=1 -s NODE_CODE_CACHING=1 -s WASM_ASYNC_COMPILATION=0`, // -s EXPORT_NAME='_libjxlem' -s MODULARIZE=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=['callMain']"`
   ]
   return args.join(" ");
 }
 
-desc("Clone dependencies");
-task("deps", [], () => {
+desc("Clone submodules");
+task("submodules", () => {
   child_process.execSync("git submodule update --init --recursive");
 });
 
+desc("Build emscripten-ports");
+task("emscripten-ports", () => {
+  child_process.execSync("embuilder build libjpeg libpng zlib");
+});
+
 desc("Configure JPEG XL command line interface");
-task("configure", ["deps"], () => {
-  const command = `emcmake cmake ${configure()}`;
+task("configure", ["submodules", "emscripten-ports"], async () => {
+  const command = `emcmake cmake ${await configure()}`;
   console.log(command);
   child_process.execSync(command, {
     env: {
@@ -53,7 +79,7 @@ task("build", () => {
   })
 });
 
-task("copy", [], async () => {
+task("copy", async () => {
   const targets = ["cjxl.js", "cjxl.wasm", "djxl.js", "djxl.wasm"];
   for (const target of targets) {
     await fs.copyFile(`build/tools/${target}`, `lib/${target}`);
